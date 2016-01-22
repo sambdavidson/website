7f9bc01aadc26c1dfceeb186faad49fcaa358949
samdamana <sdavidson@dealersocket.com>
2016-01-21 19:04:07 -0700

    changed picture to something professional

--- Client/modules/bubbleViewSwitcher/module.js
+++
@@ -1,5 +0,0 @@
-define(['./bubbleViewSwitcherController', './bubbleViewSwitcherDirective'], function(bubbleViewSwitcherController, bubbleViewSwitcherDirective) {
-    return angular.module('bubbleViewSwitcherModule', [])
-    .controller('bubbleViewSwitcherController', bubbleViewSwitcherController)
-    .directive('samBubbleViewSwitcher',bubbleViewSwitcherDirective);
-});
--- Client/modules/games/module.js
+++
@@ -1,5 +0,0 @@
-define(['./gamesController', './gamesDirective'], function(gamesController, gamesDirective) {
-   return angular.module('gamesModule', [])
-   .controller('gamesController', gamesController)
-   .directive('samGames', gamesDirective);
-});
