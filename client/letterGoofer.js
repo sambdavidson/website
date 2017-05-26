599e1f8e2f282f25e67f0e60c3af149e62503c4b
Samuel Davidson <samdamana@gmail.com>
2017-05-26 14:32:22 -0600

    Added the letter goofer to the mobile site.

--- client/letterGoofer.js
+++ client/letterGoofer.js
@@ -1,23 +1,18 @@
 (function() {
     window.addEventListener('load', function() {
-        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || !Modernizr.video) {
-            /* Mobile doesn't like web videos (I.E. the autoplay bg) so we are just going to not play anything */
-            return;
-        } else {
-            //Transform title in to a buncha letters
-            var headerEl = document.getElementById('header');
-            var title = headerEl.innerHTML.toString();
+        //Transform title in to a bunch of letters
+        var headerEl = document.getElementById('header');
+        var title = headerEl.innerHTML.toString();
 
-            headerEl.innerHTML = "";
-            headerEl.className = "noselect";
+        headerEl.innerHTML = "";
+        headerEl.className = "noselect";
 
-            for(var i = 0; i < title.length; i++) {
-                var child = document.createElement('span');
-                child.innerHTML = title.charAt(i);
-                child.className = "name-header";
-                child.onclick = letterClick;
-                headerEl.appendChild(child);
-            }
+        for(var i = 0; i < title.length; i++) {
+            var child = document.createElement('span');
+            child.innerHTML = title.charAt(i);
+            child.className = "name-header";
+            child.onclick = letterClick;
+            headerEl.appendChild(child);
         }
 
     });
