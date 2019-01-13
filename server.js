3afe43460c4a208bdc7c2b86ea6e01cb0a2ba3fc
Samuel Davidson <samdamana@gmail.com>
2019-01-12 19:04:43 -0800

    Added initial version of Paper Portraits faces page.

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 var express = require('express');
+var request = require('request');
 var app = express();
 
-app.use('/static', express.static(__dirname + '/build'));
-app.use('/static', express.static(__dirname + '/static'));
-app.use('/', express.static(__dirname + '/static'));
+//app.use('/static', express.static(__dirname + '/build'));
+//app.use('/static', express.static(__dirname + '/static'));
+//app.use('/', express.static(__dirname + '/static'));
+app.use('/', express.static(__dirname + '/build')); // Enable only this when developing
+
+app.get('/face_data.xml', function(req,res) {
+    const faceBucketUrl = 'https://storage.googleapis.com/paper-portraits-faces';
+    request(faceBucketUrl).pipe(res);
+});
+
+app.get('*', function(req, res) {
+   res.sendFile(__dirname + '/build/index.html');
+});
 
 var port = process.env.PORT || 8040;
 
